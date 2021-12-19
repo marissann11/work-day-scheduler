711cb15ad3d463d68c702f45ef2dd2f636feff47
marissann11 <wilhite.marissa@gmail.com>
2021-12-19 18:34:44 -0500

    added check time function

--- Develop/script.js
+++ Develop/script.js
@@ -10,6 +10,37 @@ displayDay.innerHTML = dayInfo;
 
 // For loop for calculating if "i" is >, <, or === current hour, add css classes accordingly
 // Grab hour (div id) from each time block and compare to "now", 
+console.log(now)
+
+function checkTime() {
+
+    console.log("time is being checked")
+
+    let currentTime = moment().hour();
+
+    $(".time-block").each(function() {
+
+        let timeBlock = parseInt($(this).attr("id"));
+
+        if (timeBlock < currentTime) {
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+        else if (timeBlock === currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
+     }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
+    })
+}
+
+setInterval(checkTime, 60000);
 
 // setInterval to make sure loop is being run often enough to  check times (?)
 
